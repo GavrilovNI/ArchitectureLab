df9321252d2d24037429ca35a5d884eb4f35121b
Nikita Gavrilov <pelmeshka1110@gmail.com>
2021-12-16 19:31:09 +0100

    removed comments

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,11 +7,8 @@ namespace Web
 {
     public class Program
     {
-        //public static DbContext context;
-
         public static void Main(string[] args)
         {
-            //context = new DbContext();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,44 +18,5 @@ namespace Web
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-            /*Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureKestrel(o =>
-                    {
-                        o.ConfigureHttpsDefaults(o =>
-                        o.ClientCertificateMode =
-                            ClientCertificateMode.RequireCertificate);
-                    });
-                });*/
     }
 }
-
-
-
-/*var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();*/
